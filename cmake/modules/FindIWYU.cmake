f3cd895320d0484b71b8429f41f01c28d5d60a4e
Hubert Degaudenzi <Hubert.Degaudenzi@unige.ch>
2023-03-03 18:14:37 +0100

    Add the FindIWYU.cmake file
    It is used to locate the include-what-you-use binary

---
+++ cmake/modules/FindIWYU.cmake
@@ -0,0 +1,17 @@
+if (NOT IWYU_FOUND)
+
+    find_program(IWYU_EXECUTABLE include-what-you-use
+                 HINTS ENV IWYU_INSTALL_DIR
+                 PATH_SUFFIXES bin)
+    set(IWYU_EXECUTABLE ${IWYU_EXECUTABLE} CACHE STRING "")
+
+# handle the QUIETLY and REQUIRED arguments and set IWYU_FOUND to TRUE if
+# all listed variables are TRUE
+    INCLUDE(FindPackageHandleStandardArgs)
+    FIND_PACKAGE_HANDLE_STANDARD_ARGS(IWYU DEFAULT_MSG IWYU_EXECUTABLE)
+
+    mark_as_advanced(IWYU_FOUND IWYU_EXECUTABLE)
+
+
+endif (NOT IWYU_FOUND)
+
