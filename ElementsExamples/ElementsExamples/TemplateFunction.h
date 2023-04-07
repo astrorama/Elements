fb9c73271d0c5395dd3a87b0e92cde801f0d2757
Hubert Degaudenzi <Hubert.Degaudenzi@unige.ch>
2023-04-07 17:28:20 +0200

    Add a template function example
    - it shows the separation between the declaration in the .h file and the
    implementation in the .tpp file
    - it shows the instanciation in the .cpp file and the declaration of
    these in the .h file

---
+++ ElementsExamples/ElementsExamples/TemplateFunction.h
@@ -0,0 +1,55 @@
+/**
+ * @file ElementsExamples/TemplateFunction.h
+ * @brief Example of a template function
+ * @date 2023-04-07
+ * @author Hubert Degaudenzi
+ *
+ * @copyright 2012-2023 Euclid Science Ground Segment
+ *
+ * This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General
+ * Public License as published by the Free Software Foundation; either version 3.0 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ */
+
+/**
+ * @addtogroup ElementsExamples ElementsExamples
+ * @{
+ */
+
+#ifndef ELEMENTSEXAMPLES_ELEMENTSEXAMPLES_TEMPLATEFUNCTION_H_
+#define ELEMENTSEXAMPLES_ELEMENTSEXAMPLES_TEMPLATEFUNCTION_H_
+
+#include "ElementsKernel/Export.h"
+
+namespace Elements {
+namespace Examples {
+
+template <typename T>
+ELEMENTS_API void templateFunction();
+
+// template instance declaration
+extern template ELEMENTS_API void templateFunction<int>();
+extern template ELEMENTS_API void templateFunction<double>();
+
+}  // namespace Examples
+}  // namespace Elements
+
+/**
+ * @def ELEMENTSEXAMPLES_ELEMENTSEXAMPLES_TEMPLATEFUNCTION_IMPL_
+ * @brief Macro to prevent the direct inclusion of the implementation file
+ */
+#define ELEMENTSEXAMPLES_ELEMENTSEXAMPLES_TEMPLATEFUNCTION_IMPL_
+#include "ElementsExamples/_impl/TemplateFunction.tpp"
+#undef ELEMENTSEXAMPLES_ELEMENTSEXAMPLES_TEMPLATEFUNCTION_IMPL_
+
+#endif  // ELEMENTSEXAMPLES_ELEMENTSEXAMPLES_TEMPLATEFUNCTION_H_
+
+/**@}*/
