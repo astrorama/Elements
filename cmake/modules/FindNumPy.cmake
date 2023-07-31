504621ca1b4bfad484edaffb457bc8986200e953
Hubert Degaudenzi <Hubert.Degaudenzi@unige.ch>
2023-07-31 15:10:07 +0200

    Add fallback method for the localisation of the Numpy include dirs

--- cmake/modules/FindNumPy.cmake
+++ cmake/modules/FindNumPy.cmake
@@ -5,15 +5,35 @@
 
 cmake_minimum_required(VERSION 3.5)
 
-
-
-if(NOT Python_NumPy_FOUND)
+if(NOT NumPy_FOUND)
   if(NumPy_FIND_QUIETLY)
     find_package(Python ${PYTHON_EXPLICIT_VERSION} COMPONENTS Interpreter Development NumPy QUIET)
   else()
     find_package(Python ${PYTHON_EXPLICIT_VERSION} COMPONENTS Interpreter Development NumPy)
     set(__numpy_out 1)
   endif()
+endif()
+
+if(NOT Python_NumPy_INCLUDE_DIRS)
+
+  if (PYTHON_EXECUTABLE)
+    # Find out the include path
+    execute_process(
+            COMMAND "${PYTHON_EXECUTABLE}" -c
+            "from __future__ import print_function\ntry: import numpy; print(numpy.get_include(), end='')\nexcept:pass\n"
+            OUTPUT_VARIABLE __numpy_path)
+    # And the version
+    execute_process(
+            COMMAND "${PYTHON_EXECUTABLE}" -c
+            "from __future__ import print_function\ntry: import numpy; print(numpy.__version__, end='')\nexcept:pass\n"
+            OUTPUT_VARIABLE Python_NumPy_VERSION)
+  elseif(__numpy_out)
+    message(STATUS "Python executable not found.")
+  endif(PYTHON_EXECUTABLE)
+
+  find_path(Python_NumPy_INCLUDE_DIRS numpy/arrayobject.h
+            HINTS "${__numpy_path}" "${PYTHON_INCLUDE_PATH}" NO_DEFAULT_PATH)
+
 endif()
 
 set(NUMPY_INCLUDE_DIRS ${Python_NumPy_INCLUDE_DIRS})
