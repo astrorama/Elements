ad584043c7bed6da5142f5bdf0b0c019895ec400
Hubert Degaudenzi <Hubert.Degaudenzi@unige.ch>
2024-04-09 16:45:53 +0200

    Final fix for the new GnuAstro API
    - detect the version of Gnuastro with CMake
    - copy the example: there is one for >= 0.22 and another otherwise
    - the new API takes an extra quiet argument

---
+++ ElementsExamples/src/program/OldGnuAstro.cpp
@@ -0,0 +1,66 @@
+/**
+ * @file OldGnuAstro.cpp
+ * @date January 6th, 2015
+ * @author Pierre Dubath
+ * @brief Version for GnuAstro version <= 0.21
+ *
+ * @copyright 2012-2020 Euclid Science Ground Segment
+ *
+ * This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General
+ * Public License as published by the Free Software Foundation; either version 3.0 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ */
+
+#include <map>     // for map
+#include <string>  // for string
+
+#include <gnuastro/cosmology.h>
+#include <gnuastro/fits.h>  // header file to test
+
+#include "ElementsKernel/Main.h"     // for MAIN_FOR
+#include "ElementsKernel/Program.h"  // for Program
+#include "ElementsKernel/Unused.h"   // for ELEMENTS_UNUSED
+
+using std::map;
+using std::string;
+
+namespace Elements {
+namespace Examples {
+
+class GnuAstro : public Program {
+
+public:
+  ExitCode mainMethod(ELEMENTS_UNUSED map<string, VariableValue>& args) override {
+
+    auto log = Logging::getLogger("GnuAstroExample");
+
+    string test_upper_string{"THATSTRING"};
+    log.info() << "This is the test upper string: " << test_upper_string;
+
+    double z{2.5};
+    double H0{67.66};
+
+    auto age = gal_cosmology_age(z, H0, 0.0, 0.0, 0.0);
+
+    log.info() << "Age of the Universe @ z = " << z << " : " << age << " GA";
+
+    return ExitCode::OK;
+  }
+};
+
+}  // namespace Examples
+}  // namespace Elements
+
+/**
+ * Implementation of a main using a base class macro
+ * This must be present in all Elements programs
+ */
+MAIN_FOR(Elements::Examples::GnuAstro)
