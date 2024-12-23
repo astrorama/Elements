22e2b6a39e7b150c9323077860aee47087937542
Hubert Degaudenzi <Hubert.Degaudenzi@gmail.com>
2024-12-23 18:31:33 +0100

    Refactor System_test and add typenameInfo_test.
    Refactored existing tests to improve clarity and const-correctness. Added a new test case, typenameInfo_test, to validate System::typeinfoName functionality for various types, enhancing coverage and reliability.

--- ElementsKernel/tests/src/System_test.cpp
+++ ElementsKernel/tests/src/System_test.cpp
@@ -33,6 +33,9 @@ using std::string;
 
 namespace Elements {
 
+template <class T>
+struct X {};
+
 BOOST_AUTO_TEST_SUITE(System_test)
 
 //-----------------------------------------------------------------------------
@@ -41,14 +44,13 @@ BOOST_AUTO_TEST_CASE(HostName_test) {
 
   using std::string;
 
-  Environment current;
-  string      DELIMITER{"."};
+  const string DELIMITER{"."};
 
-  if (current["HOSTNAME"].exists()) {
-    string full_var_hostname = current["HOSTNAME"];
-    string var_hostname      = full_var_hostname.substr(0, full_var_hostname.find(DELIMITER));
-    string full_sys_hostname = System::hostName();
-    string sys_hostname      = full_sys_hostname.substr(0, full_sys_hostname.find(DELIMITER));
+  if (Environment current; current["HOSTNAME"].exists()) {
+    string        full_var_hostname = current["HOSTNAME"];
+    const string  var_hostname      = full_var_hostname.substr(0, full_var_hostname.find(DELIMITER));
+    const string& full_sys_hostname = System::hostName();
+    const string  sys_hostname      = full_sys_hostname.substr(0, full_sys_hostname.find(DELIMITER));
     BOOST_CHECK_EQUAL(var_hostname, sys_hostname);
   }
 }
@@ -66,13 +68,73 @@ BOOST_AUTO_TEST_CASE(osName_test) {
 
 BOOST_AUTO_TEST_CASE(osVersion_test) {
 
-  string         osver{"UNKNOWN"};
+  string         os_version{"UNKNOWN"};
   struct utsname ut{};
   if (uname(&ut) == 0) {
-    osver = ut.release;
+    os_version = ut.release;
   }
 
-  BOOST_CHECK_EQUAL(System::osVersion(), osver);
+  BOOST_CHECK_EQUAL(System::osVersion(), os_version);
+}
+
+BOOST_AUTO_TEST_CASE(typenameInfo_test) {
+
+  BOOST_CHECK_EQUAL(System::typeinfoName(typeid(void).name()), "void");
+  BOOST_CHECK_EQUAL(System::typeinfoName(typeid(void)), "void");
+
+  BOOST_CHECK_EQUAL(System::typeinfoName(typeid(wchar_t).name()), "wchar_t");
+  BOOST_CHECK_EQUAL(System::typeinfoName(typeid(wchar_t)), "wchar_t");
+
+  BOOST_CHECK_EQUAL(System::typeinfoName(typeid(bool).name()), "bool");
+  BOOST_CHECK_EQUAL(System::typeinfoName(typeid(bool)), "bool");
+
+  BOOST_CHECK_EQUAL(System::typeinfoName(typeid(char).name()), "char");
+  BOOST_CHECK_EQUAL(System::typeinfoName(typeid(char)), "char");
+
+  BOOST_CHECK_EQUAL(System::typeinfoName(typeid(signed char).name()), "signed char");
+  BOOST_CHECK_EQUAL(System::typeinfoName(typeid(signed char)), "signed char");
+
+  BOOST_CHECK_EQUAL(System::typeinfoName(typeid(unsigned char).name()), "unsigned char");
+  BOOST_CHECK_EQUAL(System::typeinfoName(typeid(unsigned char)), "unsigned char");
+
+  BOOST_CHECK_EQUAL(System::typeinfoName(typeid(short).name()), "short");
+  BOOST_CHECK_EQUAL(System::typeinfoName(typeid(short)), "short");
+
+  BOOST_CHECK_EQUAL(System::typeinfoName(typeid(unsigned short).name()), "unsigned short");
+  BOOST_CHECK_EQUAL(System::typeinfoName(typeid(unsigned short)), "unsigned short");
+
+  BOOST_CHECK_EQUAL(System::typeinfoName(typeid(int).name()), "int");
+  BOOST_CHECK_EQUAL(System::typeinfoName(typeid(int)), "int");
+
+  BOOST_CHECK_EQUAL(System::typeinfoName(typeid(unsigned int).name()), "unsigned int");
+  BOOST_CHECK_EQUAL(System::typeinfoName(typeid(unsigned int)), "unsigned int");
+
+  BOOST_CHECK_EQUAL(System::typeinfoName(typeid(long).name()), "long");
+  BOOST_CHECK_EQUAL(System::typeinfoName(typeid(long)), "long");
+
+  BOOST_CHECK_EQUAL(System::typeinfoName(typeid(unsigned long).name()), "unsigned long");
+  BOOST_CHECK_EQUAL(System::typeinfoName(typeid(unsigned long)), "unsigned long");
+
+  BOOST_CHECK_EQUAL(System::typeinfoName(typeid(long long).name()), "long long");
+  BOOST_CHECK_EQUAL(System::typeinfoName(typeid(long long)), "long long");
+
+  BOOST_CHECK_EQUAL(System::typeinfoName(typeid(unsigned long long).name()), "unsigned long long");
+  BOOST_CHECK_EQUAL(System::typeinfoName(typeid(unsigned long long)), "unsigned long long");
+
+  BOOST_CHECK_EQUAL(System::typeinfoName(typeid(float).name()), "float");
+  BOOST_CHECK_EQUAL(System::typeinfoName(typeid(float)), "float");
+
+  BOOST_CHECK_EQUAL(System::typeinfoName(typeid(double).name()), "double");
+  BOOST_CHECK_EQUAL(System::typeinfoName(typeid(double)), "double");
+
+  BOOST_CHECK_EQUAL(System::typeinfoName(typeid(long double).name()), "long double");
+  BOOST_CHECK_EQUAL(System::typeinfoName(typeid(long double)), "long double");
+
+  BOOST_CHECK_EQUAL(System::typeinfoName(typeid(__float128).name()), "__float128");
+  BOOST_CHECK_EQUAL(System::typeinfoName(typeid(__float128)), "__float128");
+
+  BOOST_CHECK_EQUAL(System::typeinfoName(typeid(X<int>).name()), "Elements::X<int>");
+  BOOST_CHECK_EQUAL(System::typeinfoName(typeid(X<int>)), "Elements::X<int>");
 }
 
 //-----------------------------------------------------------------------------
